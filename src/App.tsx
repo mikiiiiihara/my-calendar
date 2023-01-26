85db6cdc85eb90ebfd52e61bf5fda3bc8e37aa54
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-01-26 10:16:59 +0900

    useEffectマウントタイミング修正

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
       unSub();
       console.log("unSubを呼び出しました");
     };
-  }, [dispatch, loading]);
+  }, [dispatch]);
 
   if (loading) return <></>;
   return (
