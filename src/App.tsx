f012dfc0b60961ee463e5a009852e875704dcde8
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-01-26 10:10:29 +0900

    loading再々チャレンジ

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
@@ -9,10 +9,12 @@ import Auth from "./components/auth/Auth";
 const App: React.FC = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     //Firebase ver9 compliant
     const unSub = onAuthStateChanged(auth, (authUser) => {
+      console.log("ログイン状態を確認しています・・・");
       if (authUser) {
         dispatch(
           login({
@@ -20,14 +22,21 @@ const App: React.FC = () => {
             displayName: authUser.displayName,
           })
         );
+        console.log("ログインしています・・・");
+        setLoading(false);
       } else {
         dispatch(logout());
+        console.log("ログアウトしています・・・");
+        setLoading(false);
       }
     });
     return () => {
       unSub();
+      console.log("unSubを呼び出しました");
     };
-  }, [dispatch]);
+  }, [dispatch, loading]);
+
+  if (loading) return <></>;
   return (
     <>
       {user.uid ? (
